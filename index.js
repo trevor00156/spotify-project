83af9f705b9eed9dcc35933e5967a21633411810
trevor00156 <kathuriarohan22@gmail.com>
2025-08-06 23:31:27 +0530

    day 3

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log("hello world");
+
+async function getsongs() {
+  let a = await fetch("http://127.0.0.1:5500/songs/");
+  let response = await a.text();
+
+  let div = document.createElement("div");
+  div.innerHTML = response;
+
+  let as = div.getElementsByTagName("a");
+
+  let songs = [];
+  console.log(as);
+
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href);
+    }
+  }
+
+  return songs;
+}
+
+async function main() {
+  let song = await getsongs();
+  console.log(song);
+}
+
+main();
