02eeaed3833271baa69ed6857148d65ac551a37c
trevor00156 <kathuriarohan22@gmail.com>
2025-08-14 00:04:33 +0530

    day6

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-console.log("hello world");
+let currentsong = null; //current song declares null
+let currentsongUrl = null; // currentsong Url declares null
 
 function formatTime(seconds) {
   const minutes = Math.floor(seconds / 60);
   const secs = Math.floor(seconds % 60);
   return `${String(minutes).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
 }
+
+let songs;
 
 // getsong function fetch the songs folder and return songs
 async function getsongs() {
@@ -17,7 +20,7 @@ async function getsongs() {
   let as = div.getElementsByTagName("a");
 
   let songs = [];
-  console.log(as);
+  // console.log(as);
 
   for (let index = 0; index < as.length; index++) {
     const element = as[index];
@@ -35,10 +38,7 @@ async function getsongs() {
 
 async function main() {
   let songList = document.querySelector(".song-list ul");
-  let songs = await getsongs(); // assuming this returns an array of song URLs
-
-  let currentsong = null; //current song declares null
-  let currentsongUrl = null; // currentsong Url declares null
+  songs = await getsongs(); // assuming this returns an array of song URLs
 
   songs.forEach((songUrl) => {
     // Extract filename from URL
@@ -95,12 +95,13 @@ async function main() {
         // listen for timeUpdate event
 
         currentsong.addEventListener("timeupdate", () => {
-          console.log(currentsong.currentTime, currentsong.duration);
-
           document.querySelector(".songDuration").innerHTML = `${formatTime(
             currentsong.currentTime
-          )}:${formatTime(currentsong.duration)}`;
+          )}/${formatTime(currentsong.duration)}`;
           document.querySelector(".songinfo").innerHTML = `${decodedName}`;
+
+          document.querySelector(".circle").style.left =
+            (currentsong.currentTime / currentsong.duration) * 100 + "%";
         });
       }
     });
@@ -118,6 +119,76 @@ async function main() {
     });
 
     songList.appendChild(li);
+  });
+
+  //Add an event listener to seekbar
+
+  document.querySelector(".seekbar").addEventListener("click", (e) => {
+    let percent = e.offsetX / e.target.getBoundingClientRect().width;
+    document.querySelector(".circle").style.left = percent * 100 + "%";
+    currentsong.currentTime = currentsong.duration * percent;
+  });
+
+  // Add an event listener to next button
+
+  next.addEventListener("click", () => {
+    console.log(songs);
+    let currentSongindex = songs.indexOf(currentsongUrl);
+    console.log(currentSongindex);
+    let nextIndex = (currentSongindex + 1) % songs.length;
+    console.log(nextIndex);
+    currentsong.pause();
+    currentsong.currentTime = 0;
+    currentsong = new Audio(songs[nextIndex]);
+    currentsongUrl = songs[nextIndex];
+
+    currentsong.play();
+    // update the name and duration of song with the next song
+    currentsong.addEventListener("timeupdate", () => {
+      let rawFileName = currentsongUrl.split("/").pop();
+      let songName = decodeURIComponent(rawFileName).replace(".mp3", "");
+
+      document.querySelector(".songinfo").innerHTML = songName;
+      document.querySelector(".songDuration").innerHTML = `${formatTime(
+        currentsong.currentTime
+      )}/${formatTime(currentsong.duration)}`;
+      document.querySelector(".circle").style.left =
+        (currentsong.currentTime / currentsong.duration) * 100 + "%";
+    });
+  });
+
+  // Add an event Listener to previous button
+
+  previous.addEventListener("click", () => {
+    console.log(songs);
+    let currentSongindex = songs.indexOf(currentsongUrl);
+    console.log(currentSongindex);
+    let prevIndex = currentSongindex - 1;
+    console.log(prevIndex);
+
+    currentsong.pause();
+
+    if (prevIndex < 0) {
+      prevIndex = songs.length - 1;
+    }
+    currentsong.currentTime = 0;
+    currentsong = new Audio(songs[prevIndex]);
+    currentsongUrl = songs[prevIndex];
+
+    currentsong.play();
+
+    // update the name and duration of song with the next song
+    currentsong.addEventListener("timeupdate", () => {
+      let rawFileName = currentsongUrl.split("/").pop();
+      let songName = decodeURIComponent(rawFileName).replace(".mp3", "");
+
+      document.querySelector(".songinfo").innerHTML = songName;
+      document.querySelector(".songDuration").innerHTML = `${formatTime(
+        currentsong.currentTime
+      )}/${formatTime(currentsong.duration)}`;
+      document.querySelector(".circle").style.left =
+        (currentsong.currentTime / currentsong.duration) * 100 + "%";
+    });
   });
 }
 
